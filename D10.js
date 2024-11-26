65bee8c41cb273f85284e415261f2d24f17db42d
Luís Gabriel <03943173003@senacrs.edu.br>
2024-11-26 09:12:16 -0300

    termineio d10

--- D10.js
+++ D10.js
@@ -6,16 +6,16 @@ function rolarDado() {
   const numero = Math.floor(Math.random() * 10) + 1;
   let x, y;
   switch (numero) {
-    case 1: x = 0; y = 0; break;
-    case 2: x = 0; y = 180; break;
-    case 3: x = 0; y = -90; break;
-    case 4: x = 0; y = 90; break;
-    case 5: x = -45; y = 0; break;
-    case 6: x = 45; y = 0; break;
-    case 7: x = 45; y = 0; break;
-    case 8: x = 45; y = 0; break;
-    case 9: x = 45; y = 0; break;
-    case 10: x = 45; y = 0; break;
+    case 1: x = 45; y = -142; break;
+    case 2: x = -45; y = 72; break;
+    case 3: x = 45; y = -70; break;
+    case 4: x = -45; y = 215; break;
+    case 5: x = 45; y = 0; break;
+    case 6: x = -45; y = 287; break;
+    case 7: x = 45; y = 145; break;
+    case 8: x = -45; y = 145; break;
+    case 9: x = 45; y = 73; break;
+    case 10: x = -45; y = 0; break;
   }
  console.log(numero);
   dado.style.transform = `rotateX(${x}deg) rotateY(${y}deg)`;
